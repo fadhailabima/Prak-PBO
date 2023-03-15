4a1429f929598bb0f2ae1bb72733fad4a213f8b5
fadhailabima <88726104+fadhailabima@users.noreply.github.com>
2023-03-15 16:38:09 +0700

    Add files via upload

---
+++ PostTest1/MTimSepakBola.java
@@ -0,0 +1,24 @@
+/**
+	*File : MTimSepakBola.java
+	*Penulis : Fadhail Athaillah Bima
+*/
+
+class MTimSepakbola {
+    public static void main(String[] args) {
+      TimSepakbola liverpool = new TimSepakbola("Liverpool");
+  
+      TimSepakbola manchesterUnited = new TimSepakbola("Manchester United");
+  
+      liverpool.startTandingBola(manchesterUnited);
+  
+      for (int i = 0; i < 7; i++) {
+        liverpool.goal();
+  
+        System.out.println("Skor sementara: " + liverpool.nama + " " + liverpool.getSkor() + " - " + manchesterUnited.getSkor() + " " + manchesterUnited.nama);
+      }
+  
+      // call endTandingBola()
+      manchesterUnited.endTandingBola(liverpool);
+    }
+  }
+  
