4aad43cae9d376468b6cb1321e36366c2ed476f0
fadhailabima <88726104+fadhailabima@users.noreply.github.com>
2023-03-21 22:57:28 +0700

    Add files via upload

---
+++ Praktikum 4/org/poligon/Poligon.java
@@ -0,0 +1,24 @@
+/*
+* File      : Poligon.java 21/03/2023
+* Penulis   : Fadhail Athaillah Bima
+* Deskripsi : representasi dasar dari objek poligon (segi banyak)
+*/
+
+package org.poligon;
+
+public class Poligon {
+  protected int jumlahSisi;
+
+  public Poligon() {
+
+  }
+
+  public void setJumlahSisi(int jumlahSisi) {
+    this.jumlahSisi = jumlahSisi;
+  }
+
+  public int getJumlahSisi() {
+    return this.jumlahSisi;
+  }
+
+}
